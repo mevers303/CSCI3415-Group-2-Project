dadec77952af0e081647f212d570dcb52982b06a
mevers303 <mevers303@gmail.com>
2025-11-23 19:07:22 -0700

    Demo #2 is dogs

--- typescript/client/src/app/demo2/demo2.ts
+++ typescript/client/src/app/demo2/demo2.ts
@@ -2,10 +2,41 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-demo2',
-  imports: [],
   templateUrl: './demo2.html',
-  styleUrl: './demo2.css',
+  styleUrls: ['./demo2.css'],
 })
 export class Demo2 {
+  images: string[] = [
+    'https://placedog.net/800/600?id=1',
+    'https://placedog.net/800/600?id=2',
+    'https://placedog.net/800/600?id=3',
+    'https://placedog.net/800/600?id=4',
+    'https://placedog.net/800/600?id=5',
+    'https://placedog.net/800/600?id=6',
+    'https://placedog.net/800/600?id=7',
+    'https://placedog.net/800/600?id=8',
+    'https://placedog.net/800/600?id=9',
+    'https://placedog.net/800/600?id=10',
+  ];
 
+  current = 0;
+
+  next() {
+    this.current = (this.current + 1) % this.images.length;
+  }
+
+  prev() {
+    this.current = (this.current - 1 + this.images.length) % this.images.length;
+  }
+
+  select(index: number) {
+    if (index >= 0 && index < this.images.length) {
+      this.current = index;
+    }
+  }
+
+  onKey(e: KeyboardEvent) {
+    if (e.key === 'ArrowRight') this.next();
+    if (e.key === 'ArrowLeft') this.prev();
+  }
 }
